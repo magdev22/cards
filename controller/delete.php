973b00ff73fae1a2086817473b226bb78b954b7a
magdev022 <magdev022@gmail.com>
2023-09-19 12:36:04 +0300

    подправил ошибку в delete php

--- controller/delete.php
+++ controller/delete.php
@@ -6,7 +6,7 @@ if ($mysqli_connect) {
         $sql = "DELETE FROM products WHERE id=" . $_GET['id'];
 
         if ($mysqli->query($sql)) {
-
+            echo "успешно добавлено";
         } else {
             echo "Ошибка: " . $mysqli->error;
         }
